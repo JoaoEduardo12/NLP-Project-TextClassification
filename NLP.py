8b9a34024238d63ff3fc1c4cd509914aee0c66dc
Eduardo Teixeira <joao.edu.t@hotmail.com>
2022-06-09 23:55:12 +0100

    NLP work to clean and retrieve information from text files.

--- NLP.py
+++ NLP.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+#from gensim.utils import Word2Vec
 
 class TextProcessing:
 	
@@ -16,9 +17,7 @@ class TextProcessing:
         self.X_test_vect = self._transform_text(self.X_test, transformer)
         if adv_bool == "True":
             import spacy
-            self.nlp = spacy.load("en_core_web_sm")
-            self.nlp.disable_pipe("parser") 
-            self.nlp.add_pipe("sentencizer")
+            self.nlp = spacy.load("en_core_web_lg")
 
     def _iterate_text(self, data_dict):
         '''
@@ -56,8 +55,8 @@ class TextProcessing:
 		to our data so we can feed it later to a ml model
 		'''
         from sklearn.feature_extraction.text import TfidfVectorizer
-        transformer = TfidfVectorizer()
-        transformed = transformer.fit_transform(array)
+        transformer = TfidfVectorizer(ngram_range = (1,2)) # filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n\'' min_df=2, stop_words='english', strip_accents='unicode', norm='l2'
+        transformed = transformer.fit_transform(array).todense()
         return transformed, transformer
 
     def _transform_text(self,array,transformer):
@@ -65,7 +64,7 @@ class TextProcessing:
 		Its important this method inherits the transformer we got from X_train,
 		so we can only use transform() and not fit_transform()
 		'''
-        return transformer.transform(array)
+        return transformer.transform(array).todense()
 
     def _get_train_test(self):
         '''
@@ -77,7 +76,7 @@ class TextProcessing:
         '''
 		Creates doc objects from spacy  in each of the clinical texts
 		'''
-        print("\nProcessing train and text datasets with spacy.\nIt will take a while..")
+        print("\nProcessing train and test datasets with SpaCy.\nIt will take a while..")
         self.X_train_doc = [self.nlp(x) for x in self.X_train_list]
         self.X_test_doc = [self.nlp(x) for x in self.X_test_list]
 
@@ -86,16 +85,16 @@ class TextProcessing:
 		Subsitutes each token by its lemma, and also gets rid of ponctuation
 		'''
         for document in range(len(self.X_train_doc) - 1):
-            self.X_train_doc[document] = [token.lemma_ for token in self.X_train_doc[document] if not token.is_punct]
+            self.X_train_doc[document] = [token.lemma_.lower() for token in self.X_train_doc[document] if not token.is_punct]
         for document in range(len(self.X_test_doc) - 1):
-            self.X_test_doc[document] = [token.lemma_ for token in self.X_test_doc[document] if not token.is_punct]
+            self.X_test_doc[document] = [token.lemma_.lower() for token in self.X_test_doc[document] if not token.is_punct]
 
-    def _add_stop_word_vocab(self, word):
+    def _add_stop_word_vocab(self, word_set):
         '''
 		Adds a specified word to the spacy english vocabolary of stop word
 		May come in handy
 		'''
-        self.nlp.Defaults.stop_words.add(word)
+        self.nlp.Defaults.stop_words.union(word_set)
 
     def _remove_stop_word_vocab(self, word):
         '''
@@ -108,18 +107,19 @@ class TextProcessing:
 		This method removes all stop words from the current documents
 		'''
         for document in range(len(self.X_train_doc) - 1):
-            self.X_train_doc[document] = [token for token in self.X_train_doc[document] if not token in self.nlp.Defaults.stop_words]
+            self.X_train_doc[document] = [token.lower() for token in self.X_train_doc[document] if not token.lower() in self.nlp.Defaults.stop_words]
         for document in range(len(self.X_test_doc) - 1):
-            self.X_test_doc[document] = [token for token in self.X_test_doc[document] if not token in self.nlp.Defaults.stop_words]
+            self.X_test_doc[document] = [token.lower() for token in self.X_test_doc[document] if not token.lower() in self.nlp.Defaults.stop_words]
 
-    def _return_processed_data(self):
+    def _return_processed_data(self, model):
         '''
 		returns the processed text to X_train and X_test
 		'''
         self.X_train_final = np.array([str(x) for x in self.X_train_doc])
         self.X_test_final = np.array([str(x) for x in self.X_test_doc])
-        self.X_train_final, transformer = self._fit_transform_text(self.X_train_final)
-        self.X_test_final = self._transform_text(self.X_test_final, transformer)
+        if model == "linear_svm" or model == "logistic_regression": 
+            self.X_train_final, transformer = self._fit_transform_text(self.X_train_final)
+            self.X_test_final = self._transform_text(self.X_test_final, transformer)
         return self.X_train_final, self.X_test_final
 
     def set_custom_boundaries(self, doc):
@@ -131,8 +131,8 @@ class TextProcessing:
                 doc[token.i+1].is_sent_start = True
         return doc
 
-    def _get_vocab_length(self):
-        return len(self.doc.vocab)
+    def _get_vocab_length(self, doc):
+        return len(doc.vocab)
 
     def _remove_sentence(self, doc, nb):
         '''
@@ -151,3 +151,19 @@ class TextProcessing:
             if token.text == string:
                 self.doc[token.i+1].is_sent_start = True
         return self.doc
+
+    def _get_max_length_docs(self):
+        max_x_train = max([len(x) for x in self.X_train_doc])
+        max_x_test = max([len(x) for x in self.X_test_doc])
+        return max(max_x_train, max_x_test)
+
+    def _clean_data(self):
+        import re
+        for document in range(len(self.X_train_doc) - 1):
+            self.X_train_doc[document] = [re.sub(r'[^ \nA-Za-z0-9À-ÖØ-öø-ÿ/]+', '', sentence) for sentence in self.X_train_doc[document]]
+            self.X_train_doc[document] = [x for x in self.X_train_doc[document] if x.isalpha()]
+            self.X_train_doc[document] = [x for x in self.X_train_doc[document] if len(x) > 2]
+        for document in range(len(self.X_test_doc) - 1):
+            self.X_test_doc[document] = [re.sub(r'[^ \nA-Za-z0-9À-ÖØ-öø-ÿ/]+', '', sentence) for sentence in self.X_test_doc[document]]
+            self.X_test_doc[document] = [x for x in self.X_test_doc[document] if x.isalpha()]
+            self.X_train_doc[document] = [x for x in self.X_train_doc[document] if len(x) > 2]
